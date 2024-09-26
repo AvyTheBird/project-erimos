ef28bfcfb1f5278ef8af69293cf442c69075bdba
avythebird <70073218+AvyTheBird@users.noreply.github.com>
2024-09-26 10:43:19 -0300

    removed Terrain3D because it was a bad idea

--- src/scenes/actors/player/player.gd
+++ src/scenes/actors/player/player.gd
@@ -2,8 +2,10 @@ extends CharacterBody3D
 
 @export var speed = 5.0
 @export var acceleration = 8.0
-@export var jump_speed = 5.0
+@export var jump_speed = 40.0
 @export var rotation_speed = 20.0
+@export var gravity = 5.0
+
 @onready var camera = $Camera
 @onready var model = $cat_model
 
@@ -17,13 +19,15 @@ func _process(delta):
 func _physics_process(delta):
 	get_move(delta)
 	move_and_slide()
-	
 	rotate_model()
 
 func get_move(delta):
 	var input = Input.get_vector("move_left","move_right","move_foward","move_backwards")
 	var dir = Vector3(input.x, 0, input.y).rotated(Vector3.UP, camera.rotation.y)
 	velocity = lerp(velocity, dir * speed, acceleration * delta)
+	
+	velocity.y -= gravity
+
 
 
 #Anim code
